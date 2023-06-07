33303d2b7200384d321a90ef880cf5f754fa9107
Something Playz <RihardsButevics@gmail.com>
2023-06-07 17:16:09 +0300

    v1

---
+++ src/routes/Galerija/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import NavBar from '$lib/NavBar.svelte';
+	import Footer from '$lib/Footer.svelte';
+	import Galerija from '$lib/Galerija.svelte';
+</script>
+
+<NavBar />
+
+<h1 class="text-5xl justify-around m-auto text-center ">GALERIJA</h1>
+
+
+
+<Galerija />
+
+
+
+
+<Footer />
---
+++ src/routes/Jaunumi/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import NavBar from '$lib/NavBar.svelte';
+	import Footer from '$lib/Footer.svelte';
+	import Jaunumi from '$lib/Jaunumi.svelte';
+
+</script>
+
+
+<NavBar />
+
+
+<Jaunumi />
+
+<Footer />
